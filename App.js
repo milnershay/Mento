432d0491b2b62fb3f087ac32d65ac199b88f1de7
milnershay <milnershay@gmail.com>
2023-05-04 23:56:14 +0300

    created menu button

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import React from "react";
+import { Factory, IconButton, Icon,AntDesign, HStack, StatusBar, Header, View, Center, Card, Popover,Button,  NativeBaseProvider, Text, Box, Heading, Slider, Container, CheckIcon } from "native-base";
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'; 
+
+
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider>
+      <StatusBar bg="#3700B3" barStyle="light-content" />
+      <Box safeAreaTop bg="gray.800">
+        <HStack  space="3" alignItems="center"  px="1" py="3"
+       justifyContent={"space-between"} w="100%">
+        <HStack>
+        <IconButton  icon={<Icon size="md" as={MaterialIcons} name="menu" color="white" />}
+          onPress={()=>{
+          console.log('menu button pressed')
+          }}
+        />
+        <Text color={"white"}  fontSize="24" fontWeight={"bold"}>Mento</Text>
+        </HStack>
+        <HStack>
+          <IconButton
+            icon={<Icon size="md" as={MaterialIcons} name="favorite" color="white" />}
+            onPress={()=>{
+            console.log('hello')
+            }}
+            
+          />
+          
+          <IconButton
+            icon={<Icon size="md" as={MaterialIcons} name="search" color="white" />}
+            onPress={()=>{
+            console.log('hello')
+            }}
+            
+          />
+          
+          <IconButton
+            icon={<Icon size="md" as={MaterialIcons} name="more-vert" color="white" />}
+            onPress={()=>{
+            console.log('hello')
+            }}
+            
+          />
+          
+          
+          
+          
+        </HStack>     
+        </HStack>
+      </Box>
+      
+      
+      
+    </NativeBaseProvider>
+  ); 
+
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +69,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
