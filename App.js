41ebe9c5ed7ce0d0455089150d6fbd5424715749
milnershay <milnershay@gmail.com>
2023-05-06 18:42:08 +0300

    created signup and login page

--- App.js
+++ App.js
@@ -1,73 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet} from 'react-native';
 import React from "react";
-import { Factory, IconButton, Icon,AntDesign, HStack, StatusBar, Header, View, Center, Card, Popover,Button,  NativeBaseProvider, Text, Box, Heading, Slider, Container, CheckIcon } from "native-base";
+import {Image, extendTheme, Factory, IconButton, Icon,AntDesign, HStack, StatusBar, Header, View, Center, Card, Popover,Button,  NativeBaseProvider, Text, Box, Heading, Slider, Container, CheckIcon } from "native-base";
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'; 
-
-
+import {login} from './views/login.js'
+import { NavigationContainer } from "@react-navigation/native";
 
 
 export default function App() {
 
   return (
     <NativeBaseProvider>
-      <StatusBar bg="#3700B3" barStyle="light-content" />
-      <Box safeAreaTop bg="pink.500">
-        <HStack  space="3" alignItems="center"  px="1" py="3"
-       justifyContent={"space-between"} w="100%">
-        <HStack>
-        <IconButton  icon={<Icon size="md" as={MaterialIcons} name="menu" color="white" />}
-          onPress={()=>{
-          console.log('menu button pressed')
-          }}
-        />
-        <Text color={"white"}  fontSize="24" fontWeight={"bold"}>Mento</Text>
-        </HStack>
-        <HStack>
-          <IconButton
-            icon={<Icon size="md" as={MaterialIcons} name="favorite" color="white" />}
-            onPress={()=>{
-            console.log('hello')
-            }}
-            
-          />
-          
-          <IconButton
-            icon={<Icon size="md" as={MaterialIcons} name="search" color="white" />}
-            onPress={()=>{
-            console.log('hello')
-            }}
-            
-          />
-          
-          <IconButton
-            icon={<Icon size="md" as={MaterialIcons} name="more-vert" color="white" />}
-            onPress={()=>{
-            console.log('hello')
-            }}
-            
-          />
-          
-          
-          
-          
-        </HStack>     
-        </HStack>
-      </Box>
-      
-      
-      
+        <login/>
     </NativeBaseProvider>
   ); 
 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 
