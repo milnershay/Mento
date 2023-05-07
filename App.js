7f7fd318835513144e746b49a2ee5cf44f962945
milnershay <milnershay@gmail.com>
2023-05-07 11:10:45 +0300

    fixed few bugs, changed typescript to javascript

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StyleSheet} from 'react-native';
 import React from "react";
 import {Image, extendTheme, Factory, IconButton, Icon,AntDesign, HStack, StatusBar, Header, View, Center, Card, Popover,Button,  NativeBaseProvider, Text, Box, Heading, Slider, Container, CheckIcon } from "native-base";
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'; 
-import {login} from './views/login.js'
+import {Login} from './views/login.js'
 import { NavigationContainer } from "@react-navigation/native";
 
 
@@ -10,7 +10,7 @@ export default function App() {
 
   return (
     <NativeBaseProvider>
-        <login/>
+        <Login/>
     </NativeBaseProvider>
   ); 
 
